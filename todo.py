ea0a396a114018545f44b9fbdc2e27293a15e53d
dell <maqsad.novruzov@gmail.com>
2020-07-08 00:09:48 +0400

    Initial Upload

--- todo.py
+++ todo.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,7 +7,47 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    todos = Todo.query.all()
+    """
+    [
+        {"id":1,"title":"Test1","content":"Test1,"complete"=0}
+    ]
+    """
+    return render_template("index.html",todos=todos)
+
+@app.route("/add",methods=["POST"])
+def addTodo():
+    title =  request.form.get("title")
+    content = request.form.get("content")
+    newTodo = Todo(title = title,content = content, complete = False)
+
+    db.session.add(newTodo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/complete/<string:id>")
+def completeTodo(id):
+    todo = Todo.query.filter_by(id=id).first()
+    if todo.complete == False:
+        todo.complete = True
+    else:
+        todo.complete = False
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/delete/<string:id>")
+def deleteTodo(id):
+    todo = Todo.query.filter_by(id=id).first()
+    db.session.delete(todo)
+    db.session.commit()
+
+    return redirect(url_for("index"))
+
+@app.route("/detail/<string:id>")
+def detailTodo(id):
+    todo = Todo.query.filter_by(id=id).first()
+
+    return render_template("detail.html",todo=todo)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
